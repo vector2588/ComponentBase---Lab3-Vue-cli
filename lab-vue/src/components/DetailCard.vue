6c00b9defbc70ef3dc0f5a180e9f44e2896e7bab
Vector2588 <natkamol082@gmail.com>
2021-07-15 16:45:36 +0700

    Question 3.12 create new component

---
+++ lab-vue/src/components/DetailCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="detail-card">
+        <h4>Category: {{ event.category }}</h4>
+        <h4>Organizer: {{ event.organizer }}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DetailCard',
+    props: {
+        event: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.detail-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+    text-align: right;
+}
+
+.detail-card:hover {
+    transform:scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
