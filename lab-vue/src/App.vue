48786d28965c4b8c95997b8b5c80d458ad4d52d1
Vector2588 <natkamol082@gmail.com>
2021-07-15 19:25:13 +0700

    Question 4.6 new page

--- lab-vue/src/App.vue
+++ lab-vue/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/page">Page</router-link>
   </div>
+  
   <router-view />
 </template>
 
--- lab-vue/src/views/EventList.vue
+++ lab-vue/src/views/EventList.vue
@@ -1,20 +1,18 @@
 <template>
+  <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-    <DetailCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue"
-import DetailCard from "@/components/DetailCard.vue"
 
 export default {
-  name: "Home",
+  name: "EventList",
   components: {
     EventCard, //register it as a child component
-    DetailCard
   },
   data() {
     return {
@@ -57,6 +55,7 @@ export default {
   }
 }
 </script>
+
 <style scoped>
 .events {
   display: flex;
